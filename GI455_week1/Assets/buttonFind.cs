903d51610f0d678d7884a58489c6d77b14da640d
DESKTOP-QL0GKHM\Admin <supamit.rerk@bumail.net>
2021-01-29 17:56:44 +0700

    Update project

---
+++ GI455_week1/Assets/buttonFind.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonFind : MonoBehaviour
+{
+
+    List<string> nameCars = new List<string>() { "Toyota", "Honda", "Mazda", "Nissan", "BMW", };
+    public List<Text> textCars = new List<Text>();
+    public InputField InputFieldText;
+    public GameObject output;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InputFieldText.onEndEdit.AddListener(FindText);
+        for(int i = 0; i <= nameCars.Count - 1; i++)
+        {
+            textCars[i].text = nameCars[i] .ToString();
+        }
+    }
+
+    public void FindText(string textCars)
+    {
+        if (nameCars.Contains(InputFieldText.text))
+        {
+            output.GetComponent<Text>().text = "[ " + textCars + " ]" + " is found. ";
+        }
+        else
+        {
+            output.GetComponent<Text>().text = "[ " + textCars + "] " + " is not found. ";
+        }
+    }
+
+}
